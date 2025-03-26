905041a4151ddf7906ed7cc4f4f5d15aa4ce7525
plynch <94209622+plnw@users.noreply.github.com>
2025-03-26 06:08:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ def upload():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
