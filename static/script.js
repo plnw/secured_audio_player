c141d811f1014fa8bac8191af6e5c77c456cf17a
Natnicha Kittidilok <168412576+malxmild@users.noreply.github.com>
2025-03-29 11:31:15 +0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+// Wait until the HTML document is fully loaded before executing the scrip
 document.addEventListener("DOMContentLoaded", function() {
+    // Log a message to the console to indicate that the Secured Audio Player has loaded
     console.log("Secured Audio Player Loaded");
 });
